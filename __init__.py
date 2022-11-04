b8233c1f40628cbe7380f15cf88f1264bfe0a737
Henry Post <HenryFBP@gmail.com>
2022-11-04 02:13:47 +0000

    get dialog to work

--- __init__.py
+++ __init__.py
@@ -5,9 +5,9 @@ class MinecraftGame(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
 
-    @intent_file_handler('blockgame.intent.start_game')
+    @intent_file_handler('start.game.intent')
     def handle_game_start(self, message):
-        self.speak_dialog('blockgame.response.starting_game')
+        self.speak_dialog('starting.game')
 
 
 def create_skill():
